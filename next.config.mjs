923565556fc46e3d6353845052db62f2c209862d
byeoru <byeoru215@gmail.com>
2024-03-29 19:37:39 +0900

    logging, experimental setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    taint: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
 
 export default nextConfig;
