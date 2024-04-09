e26c35d1410b1b14bca26a70f8cdfe093f28f292
byeoru <byeoru215@gmail.com>
2024-04-10 01:00:15 +0900

    margin 으로인해 스크롤시 top bar 가 밀려 올라가는 문제를 해결

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           />
         </nav>
       </div>
-      <div className="w-full flex h-[calc(100vh-4.5rem)] sm:h-[calc(100vh-2rem)] lg:h-[calc(100vh-2rem)]">
+      <div className="w-full flex h-[calc(100vh-4.5rem)] sm:h-[calc(100vh-2rem)] lg:h-[calc(100vh-3rem)]">
         <nav className="pl-3 pr-3 py-2 overflow-y-auto hidden sm:flex flex-col lg:w-48">
           <SideNavigationGroup>
             <SideNavigationItem
