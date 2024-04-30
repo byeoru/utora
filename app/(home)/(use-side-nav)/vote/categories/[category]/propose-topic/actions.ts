c08ee04a8b192846b1b5d5f4737c699e6053fcf9
byeoru <byeoru215@gmail.com>
2024-04-30 19:18:04 +0900

    prisma error parsing

--- app/(home)/(use-side-nav)/vote/categories/[category]/propose-topic/actions.ts
+++ app/(home)/(use-side-nav)/vote/categories/[category]/propose-topic/actions.ts
@@ -4,6 +4,7 @@ import getSession from "@/lib/session";
 import { topicProposeSchema } from "./schema";
 import db from "@/lib/db";
 import { notFound } from "next/navigation";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export async function createTopic(formData: FormData) {
   const validation = topicProposeSchema.safeParse({
@@ -32,6 +33,17 @@ export async function createTopic(formData: FormData) {
       },
     });
   } catch (error) {
-    return notFound();
+    if (error instanceof PrismaClientKnownRequestError) {
+      // Prisma가 인식하는 특정 오류 코드에 대한 처리
+      console.log(error);
+      if (error.code === "P2025") {
+        // User table과 connect에 실패한 경우
+        session.destroy();
+        return notFound();
+      }
+    } else {
+      // Prisma가 알 수 없는 다른 종류의 오류 처리
+      console.error("Unknown Prisma Error:", error);
+    }
   }
 }
